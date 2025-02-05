3e41a4db911c6697510e887b37b197b5a7e8f28c
FadwaElouah <fadwaelouah@gmail.com>
2025-02-05 14:57:25 +0100

    page darabase

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'host' => 'localhost',
+    'port' => '5432',
+    'dbname' => 'cabinet_medical',
+    'username' => 'votre_utilisateur',
+    'password' => 'votre_mot_de_passe'
+];
+
