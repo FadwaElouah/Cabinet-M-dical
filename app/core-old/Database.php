436110850874e45f60da8a9d233d2f67430b6edc
FadwaElouah <fadwaelouah@gmail.com>
2025-02-05 12:24:36 +0100

    add page controller database core-old

---
+++ app/core-old/Database.php
@@ -0,0 +1,25 @@
+<?php
+class Database
+{
+    private $pdo;
+
+    public function __construct()
+    {
+        $config = require_once '../config/database.php';
+        $dsn = "pgsql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}";
+        try {
+            $this->pdo = new PDO($dsn, $config['username'], $config['password']);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Erreur de connexion : " . $e->getMessage());
+        }
+    }
+
+    public function query($sql, $params = [])
+    {
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($params);
+        return $stmt;
+    }
+}
+
