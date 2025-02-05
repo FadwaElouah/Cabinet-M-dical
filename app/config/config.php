e0c5a8d6aa37e78a08caf5bd999d9cac11e5b98a
FadwaElouah <fadwaelouah@gmail.com>
2025-02-05 11:59:41 +0100

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'port' => '5432', // Port par défaut de PostgreSQL
+        'dbname' => 'cabinet_medical',
+        'user' => 'votre_utilisateur_postgresql',
+        'password' => 'votre_mot_de_passe_postgresql',
+    ],
+    // Autres configurations...
+];
